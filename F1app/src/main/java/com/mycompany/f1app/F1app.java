699f871032cd8351e1e2f468a960c5c807b3f8c2
Tanguyhlt <tghtravail@gmail.com>
2026-03-12 10:37:42 +0100

    Mise à jour du fichier [Nom du fichier] avec les nouveautés

---
+++ F1app/src/main/java/com/mycompany/f1app/F1app.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.f1app;
+
+/**
+ *
+ * @author Admin
+ */
+public class F1app {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
